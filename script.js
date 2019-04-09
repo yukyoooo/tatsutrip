dad477a1e972cbc2e470be898e22790ae0d2e87c
yukyoooo <45087034+yukyoooo@users.noreply.github.com>
2019-04-09 22:08:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function(){
+  //この中にjQueryを書く
+
+  $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+
+    } else {
+      $answer.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+
+    }
+  });
+
+
+
+
+
+
+
+});
